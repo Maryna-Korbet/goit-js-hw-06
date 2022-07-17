a185eb04a3558d928bac351c1641fc946da94e47
Maryna-Korbet <m22v86k@gmail.com>
2022-07-17 06:05:11 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -2,3 +2,34 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const refs = {
+  input: document.querySelector("input"),
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
+  boxesDiv: document.querySelector("#boxes"),
+};
+
+refs.createBtn.addEventListener('click', createBoxes);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function createEl(amount) {
+    const basicSize = 20;
+    for (let i = 0; i < amount; i += 1) {
+    const size = basicSize + i * 10;
+    let divEl = document.createElement("div");
+    divEl.textContent = `${i + 1}`;
+    divEl.style.width = `${size}px`;
+    divEl.style.height = `${size}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    refs.boxesDiv.appendChild(divEl);
+  }
+};
+
+function createBoxes(){
+  createEl(refs.input.value);
+}
+
+function destroyBoxes() {
+  refs.boxesDiv.innerHTML = "";
+}
+
